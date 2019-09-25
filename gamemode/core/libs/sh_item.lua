eb66f6186f38fecfc638ea8f9318f20ffb12366d
ts-co <3075268+ts-co@users.noreply.github.com>
2019-09-25 00:55:25 -0400

    Update sh_item.lua

--- gamemode/core/libs/sh_item.lua
+++ gamemode/core/libs/sh_item.lua
@@ -19,8 +19,6 @@ function nut.item.load(path, baseID, isBaseItem)
 	if (uniqueID) then
 		uniqueID = (isBaseItem and "base_" or "")..uniqueID
 		local item = nut.item.register(uniqueID, baseID, isBaseItem, path)
-		
-		hook.Run("OnItemLoaded", item)
 	elseif (!path:find(".txt")) then
 		ErrorNoHalt(
 			"[NutScript] Item at '"..path.."' follows invalid "..
